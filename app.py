27944de55dcf9cc6a785a071b23d95be8a403fc6
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-02-20 12:28:34 +0330

    init the project

---
+++ app.py
@@ -0,0 +1,88 @@
+import json
+import base64
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import mysql.connector
+import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+# Configure logging to display messages in terminal
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Load website URLs from JSON file
+with open('websites.json') as f:
+    websites = json.load(f)
+
+# Setup Selenium
+chrome_options = webdriver.ChromeOptions()
+chrome_options.set_capability('browserless:token', '0iUMlmDN_NB6YWDQ1RF')
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Remote(
+    command_executor='https://headless.liara.run/webdriver',
+    options=chrome_options)
+
+# Connect to MariaDB
+db_connection = mysql.connector.connect(
+    host="tai.liara.cloud",
+    port=30983,
+    user="root",
+    password="seh1iWk2MvRySPWhUHp01m1N",
+    database="trusting_merkle"
+)
+db_cursor = db_connection.cursor()
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/run":
+            # Run your Python script here
+            logging.info(f"Be Happy! Script is running...")
+            check_table()
+            take_and_save_screenshots()
+            db_cursor.close()
+            db_connection.close()
+            driver.quit()
+            logging.info(f"All done! Time to rest.")
+
+            # Send a response to indicate the script has been executed
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Python script executed successfully')
+
+
+def check_table():
+    db_cursor.execute("SHOW TABLES LIKE 'screenshots'")
+    if not db_cursor.fetchone():
+        db_cursor.execute("CREATE TABLE screenshots (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), screenshot MEDIUMTEXT)")
+
+# Function to take screenshots and save to database
+def take_and_save_screenshots():
+    try:
+        # Iterate over websites
+        for website in websites:
+            url = website['url']
+            driver.get(url)
+
+            # Take screenshot
+            screenshot = driver.get_screenshot_as_png()
+
+            # Convert screenshot to base64
+            screenshot_base64 = base64.b64encode(screenshot).decode('utf-8')
+
+            # Save base64 screenshot to MariaDB
+            query = "INSERT INTO screenshots (url, screenshot) VALUES (%s, %s)"
+            values = (url, screenshot_base64)
+            db_cursor.execute(query, values)
+            db_connection.commit()
+
+            logging.info(f"Screenshot saved for {url}")
+    except Exception as e:
+        logging.error(f"Error occurred: {str(e)}")
+
